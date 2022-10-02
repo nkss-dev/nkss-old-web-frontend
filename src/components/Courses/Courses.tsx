9ad4d53ee53f4d70ccfff95b299cc48c89e19949
Yash Mittal <mittal.yash12@hotmail.com>
2022-10-02 16:10:57 +0530

    basic course searching code
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -2,6 +2,17 @@ import * as React from "react";
 import "./courses.scss";
 
 const Courses = () => {
+  const [courses, setCourses] = React.useState([]);
+  const [setFilteredCourses, setFilteredCourses] = React.useState([]);
+
+  const getSearch(query: string) {
+    let searchedCourses = [] ;
+    courses.filter((course) => {
+      return course.name.contains(query) || course.code == query ;
+    })
+  }
+
+
   return <div></div>;
 };
 
