27dbaf613e6af257eafd3365119dc8cdac182b6f
Yash Mittal <mittal.yash12@hotmail.com>
2022-10-05 16:47:31 +0530

    removed non required components
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Announcements from "./components/Announcements";
+// import Announcements from "./components/Announcements";
 import Home from "./components/Home";
-import Notes from "./components/Notes";
-import Sidebar from "./components/Sidebar";
+// import Notes from "./components/Notes";
+// import Sidebar from "./components/Sidebar";
 import Login from "./components/Login/Login";
 import Clubs from "./components/Clubs/Clubs";
 import Courses from "./components/Courses/Courses";
@@ -13,15 +13,12 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <div>
-          {" "}
-          <Sidebar />{" "}
-        </div>
+        <div> {/* <Sidebar />{" "} */}</div>
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/announcements" element={<Announcements />} />
-            <Route path="/notes" element={<Notes />} />
+            {/* <Route path="/announcements" element={<Announcements />} /> */}
+            {/* <Route path="/notes" element={<Notes />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/clubs" element={<Clubs />} />
             <Route path="/courses" element={<Courses />} />
