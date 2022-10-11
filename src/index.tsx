961ce2afc47c146fc47d75c22837950c77f05033
Yash Mittal <mittal.yash12@hotmail.com>
2022-10-11 14:06:48 +0530

    Update: Updated local host URL in index.tsx to hostel api
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import './index.scss';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./index.scss";
+import App from "./App";
 
-
-axios.defaults.baseURL = `http://localhost:8081/`;
-axios.defaults.headers.post['Content-Type'] = 'application/vnd.api+json';
+axios.defaults.baseURL = `http://api.nksss.live`;
+axios.defaults.headers.post["Content-Type"] = "application/vnd.api+json";
 axios.defaults.withCredentials = true;
-axios.defaults.headers.get['Authorization'] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsInJvbGxubyI6IjEyMDIyMDA1In0.9Btng_hYvKwIjeYS88zAolwiKfuRJE4Q71LMT_lK9jI`
+axios.defaults.headers.get[
+  "Authorization"
+] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsInJvbGxubyI6IjEyMDIyMDA1In0.9Btng_hYvKwIjeYS88zAolwiKfuRJE4Q71LMT_lK9jI`;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
