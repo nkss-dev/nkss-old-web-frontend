f680a673d49c019c78300c87d8b32779b87a90e6
Yash Mittal <mittal.yash12@hotmail.com>
2022-10-14 12:08:02 +0530

    Fix: Fixed course list page css
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -4,10 +4,14 @@ import CourseCard from "./CourseCard";
 import Axios from "axios";
 import "./courses.scss";
 import CourseSearchBar from "./CourseSearchBar";
+import AnnouncementNotoCard from "../AnnouncementNotoCard";
 
 const Courses = () => {
   const [courses, setCourses] = React.useState<any[]>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
   const [filteredCourses, setFilteredCourses] = React.useState([]);
+  const [branch, setBranch] = React.useState<any>("All Branches");
+  const [degree, setDegree] = React.useState<any>("All Degree");
 
   const getSearch = (query: string) => {
     let searchedCourses: Course[] = [];
@@ -26,6 +30,7 @@ const Courses = () => {
         withCredentials: true,
       }).then((res: any) => {
         setCourses([...res.data.data]);
+        setIsLoading(false);
       });
     } catch (err) {
       alert(err);
@@ -33,17 +38,79 @@ const Courses = () => {
   }, []);
 
   return (
-    <div>
-      <div className="headingConatiner"> Courses </div>
-      <div>
-        <CourseSearchBar />
-      </div>
-      <div>
-        {courses.map((course: any) => {
-          return <CourseCard code={course.code} title={course.title} />;
-        })}
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <> Loading ... </>
+      ) : (
+        <div className="announcementCont container-fluid">
+          <div className="header container">
+            <h1>Announcements</h1>
+            <p>
+              Stay updated with latest college announcement. Filter unwanted
+              announcements as per your discipline.
+            </p>
+          </div>
+          <div className="container partition"></div>
+          <div className="filters container">
+            <select
+              onChange={(e) => setBranch(e.target.value)}
+              name="branch"
+              id="branch"
+              defaultValue={"All Branches"}
+            >
+              <option value="All Branches">All Branches</option>
+              <option value="CS">CS</option>
+              <option value="IT">IT</option>
+              <option value="EC">ECE</option>
+              <option value="EE">EE</option>
+              <option value="ME">ME</option>
+              <option value="PI">PIE</option>
+              <option value="CE">Civil</option>
+            </select>
+            <select
+              onChange={(e) => setDegree(e.target.value)}
+              name="degree"
+              id="degree"
+              defaultValue={"All Courses"}
+            >
+              <option value="All Courses">All Courses</option>
+              <option value="B.Tech.">B.Tech</option>
+              <option value="M.Tech.">M.Tech</option>
+              <option value="MCA">MCA</option>
+            </select>
+          </div>
+          <div className="notificationCont container">
+            <div className="row">
+              {courses.length === 0 ? (
+                // <h3 className="noNoto"> No notifications available </h3>
+                <h3 className="noNoto">Loading ...</h3>
+              ) : (
+                <>
+                  {courses.map((e: any) => {
+                    return (
+                      <>
+                        {e.title === "" ? (
+                          ""
+                        ) : courses.length === 0 ? (
+                          <> Loading </>
+                        ) : (
+                          <AnnouncementNotoCard
+                            date={e.code}
+                            desc={e.title}
+                            link={`https://nksss.live/courses/${e.code}`}
+                            className="col-1"
+                          />
+                        )}
+                      </>
+                    );
+                  })}
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
