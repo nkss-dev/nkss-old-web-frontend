b6bd7bb99cf54af8b380d3828aa994e10ad08ac9
GetPsyched <priyanshutr@gmail.com>
2022-10-15 01:13:22 +0530

    Update home page text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../style/home.scss";
 import AnnouncementCard from "./AnnouncementCard";
 import rightArrow from "../assets/rightArrow.png";
-import underline from "../assets/underline.png";
 import CoursesCard from "./CoursesCard";
 import ClubsCard from "./ClubsCard";
 import { Link, useNavigate } from "react-router-dom";
@@ -13,19 +12,25 @@ function Home() {
     <div className="homeCont container-fluid">
       <div className="header container">
         <h1>NIT-KKR Student Support System</h1>
+        <h2>v1-alpha</h2>
         <p>
-          Welcome! The NKSSS team aims to solve as many issues you face in our college as technologically possible!
+          Welcome! The <a href="/about">NKSSS team</a> aims to solve as many issues you face in our college as technologically possible!
           The following 3 options are ones that we're currently actively developing.
           This website is currently under development and you will be notified once its full release rolls around!
+          <br></br>
+          <br></br>
+          PS: NKSSS is <em>not</em> affiliated with NITKKR and is completely student run.
         </p>
       </div>
-      <div className="SignUpCont">
+      <br></br>
+      <br></br>
+      {/* <div className="SignUpCont">
         <Link to="/about">
           <button className="signUp">
             About Us <img src={rightArrow} alt="" />
           </button>
         </Link>
-      </div>
+      </div> */}
       <div className="cardsCont container">
         <div className="row">
           <div onClick={() => navigate("/announcements")} className="col-md-3">
